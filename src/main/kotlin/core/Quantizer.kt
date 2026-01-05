99fceff644a37c8afe6c8ed42d5f9d73523facec
Bingergie <i@hebing.me>
2026-01-05 18:59:45 +0800

    created functions in Quantizer.kt

--- src/main/kotlin/core/Quantizer.kt
+++ src/main/kotlin/core/Quantizer.kt
@@ -1,7 +1,37 @@
 package core
 
+import kotlin.Int
+import kotlin.String
+import kotlin.math.abs
+import kotlin.math.roundToInt
+
 /**
  * Provides functions that quantize durations and anchor ticks.
  */
-class Quantizer {
+class Quantizer(val ticksPerQuarterNote: Int) {
+    private val quantizedDurationsInTicks: Map<Int, String> = mapOf(
+        ticksPerQuarterNote * 4 to "whole",
+        ticksPerQuarterNote * 2 to "half",
+//            ticksPerQuarterNote * 3 / 2 to "half",
+        ticksPerQuarterNote * 1 to "quarter",
+//            ticksPerQuarterNote * 2 / 3 to "quarter",
+        ticksPerQuarterNote / 2 to "eighth",
+//            ticksPerQuarterNote / 3 to "eighth",
+        ticksPerQuarterNote / 4 to "16th",
+//            ticksPerQuarterNote / 6 to "16th",
+//            ticksPerQuarterNote / 8 to "32nd",
+    )
+
+    fun getClosestNoteDurationAndType(durationInTicks: Long): Pair<Int, String> {
+        val closestDuration = quantizedDurationsInTicks.keys.minBy { durations -> abs(durationInTicks - durations) }
+        return Pair(closestDuration, quantizedDurationsInTicks[closestDuration] as String)
+    }
+
+    fun getClosestQuantizedAnchorTick(anchorTick: Long): Int {
+        val closestTickDivision = quantizedDurationsInTicks.keys.minBy { durations -> abs(anchorTick % durations) }
+        val quantizedTick = (anchorTick / closestTickDivision.toDouble()).roundToInt() * closestTickDivision
+        return quantizedTick
+    }
+
+
 }
