e6ec127abe7ce7dde2791cfa3a506ee5c20587e0
Bingergie <i@hebing.me>
2026-01-05 21:40:38 +0800

    added comments

--- src/main/kotlin/core/ScoreDesigner.kt
+++ src/main/kotlin/core/ScoreDesigner.kt
@@ -12,6 +12,7 @@ class ScoreDesigner {
     fun transformScore(score: Score) {
         currentScore = score
         quantizer = Quantizer(currentScore!!.ticksPerQuarterNote)
+        // order sometimes matter... I know it's bad but it works right now
         addNoteNotationStepAndAlter()
         quantizeNotes()
         addNoteNotationChordAndVoice()
@@ -25,11 +26,15 @@ class ScoreDesigner {
         val alterLookup = listOf(null, 1, null, 1, null, null, 1, null, 1, null, 1, null)
         var currentKeySignature: KeySignature =
             KeySignature(0, 0, KeySignature.Mode.MAJOR) // default key signature is C major
+
+        // loop over every note in every staff
         for (currentStaff in currentScore!!.staves) {
             for (staffSymbol in currentStaff.staffSymbols) {
                 when (staffSymbol) {
                     is Note -> {
                         val note = staffSymbol
+
+                        // set step and alter
                         note.notationInfo.step = stepLookup[note.pitch % 12]
                         note.notationInfo.alter = alterLookup[note.pitch % 12]?.toBigDecimal()
                     }
@@ -44,7 +49,8 @@ class ScoreDesigner {
     private fun quantizeNotes() {
         assert(currentScore != null)
         assert(quantizer != null)
-        currentScore!!.ticksPerQuarterNote
+
+        // loop over every note in every staff
         for (currentStaff in currentScore!!.staves) {
             for (staffSymbol in currentStaff.staffSymbols) {
                 when (staffSymbol) {
@@ -68,11 +74,13 @@ class ScoreDesigner {
     private fun addNoteNotationChordAndVoice() {
         for (staff in currentScore!!.staves) {
             val notes = staff.staffSymbols.filterIsInstance<Note>()
+
+            // reset previous note
             var previousNote: Note? = null
             notes.forEach { note ->
-                val currentAnchorTick = note.anchorTick
-                val previousAnchorTick = previousNote?.anchorTick
-                if (currentAnchorTick == previousAnchorTick) {
+                // note is part of chord if it has the same anchor tick as previous note
+                // todo: this breaks if the two notes have different lengths
+                if (note.anchorTick == previousNote?.anchorTick) {
                     note.notationInfo.isChord = true
                 }
                 previousNote = note
@@ -81,24 +89,36 @@ class ScoreDesigner {
     }
 
     private fun addRests() {
+        // store last note to fill in until last note later
         val lastNoteOfScoreEndTick = currentScore!!.staves.maxOf { staff -> staff.staffSymbols.maxOf { it.anchorTick } }
         currentScore!!.staves.forEach { staff ->
-            var previousNote: Note? = Note(0L, 1, 0L, 0)
+
+            // clear previous note at beginning of staff
+            var previousNote: Note = Note(0L, 1, 0L, 0)
+
+            // loop over every note in every staff
             for (index in 0 until staff.staffSymbols.size) {
                 when (val staffSymbol = staff.staffSymbols[index]) {
                     is Note -> {
                         val currentNote = staffSymbol
-                        if (!(currentNote.notationInfo.isChord || previousNote == null)) {
+
+                        // add rest if there is a gap between previous and current note
+                        if (!(currentNote.notationInfo.isChord)) { // not part of chord
                             val previousNoteEndTick = previousNote.anchorTick + previousNote.durationInTicks
                             val restDurationInTicks = currentNote.anchorTick - previousNoteEndTick
-                            if (restDurationInTicks > 0) {
+                            if (restDurationInTicks > 0) { // there is a gap between previous and current note
+
+                                // create rest that fills this gap
                                 val rest = Rest(previousNoteEndTick, restDurationInTicks).apply {
+                                    // quantize the rest (maybe redundant since this function is called after quantize notes; restType is missing, but it is not required by musicxml)
                                     val (closestDuration, closestDurationType) = quantizer!!.getClosestNoteDurationAndType(durationInTicks)
                                     this.notationInfo.restType = closestDurationType
                                     this.quantizedDurationInTicks = closestDuration
                                     val closestAnchorTick = quantizer!!.getClosestQuantizedAnchorTick(anchorTick)
                                     this.quantizedAnchorTick = closestAnchorTick
                                 }
+
+                                // insert rest after between previous and current note
                                 staff.staffSymbols.add(index, rest)
                             }
                         }
@@ -106,6 +126,8 @@ class ScoreDesigner {
                     }
                 }
             }
+
+            // add rest if there is gap between last note of staff and last note of score
             val lastNoteOfStaff = staff.staffSymbols.lastOrNull {it is Note} as Note
             val lastNoteOfStaffEndTick = lastNoteOfStaff.anchorTick + lastNoteOfStaff.durationInTicks
             if (lastNoteOfStaffEndTick < lastNoteOfScoreEndTick) {
